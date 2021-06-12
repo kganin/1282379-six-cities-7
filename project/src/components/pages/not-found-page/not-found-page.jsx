52bfc2b83f52776cd6ba2bce00c1058ab9113df2
kganin <kolyaganin@outlook.com>
2021-06-12 10:25:44 +0300

    Добавляет компонент Header

--- project/src/components/pages/not-found-page/not-found-page.jsx
+++ project/src/components/pages/not-found-page/not-found-page.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from '../../header/header';
 
 function NotFoundPage() {
   return (
-    <div>
-      <h1>404. Page not found</h1>
-      <a href="/">Вернуться на главную</a>
+    <div className="page">
+      <Header />
+      <main className="page__main" style={{textAlign: 'center', padding: '40px'}}>
+        <h1>404. Page not found</h1>
+        <a href="/">Вернуться на главную</a>
+      </main>
     </div>
   );
 }
